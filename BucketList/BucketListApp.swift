51461a996907df1ed81d4287fe49f3219274720d
Saydulayev <saydulayev.wien@gmail.com>
2024-12-26 10:32:23 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Saydulayev on 26.12.24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
